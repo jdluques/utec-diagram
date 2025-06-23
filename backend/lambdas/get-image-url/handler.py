5ad3f84b45c4b5d4ddcaca572723f6ae9a7f1989
jdluques <juan.luque@utec.edu.pe>
2025-06-22 20:41:41 -0500

    feat: add function to get image url from S3 Bucket

---
+++ backend/lambdas/get-image-url/handler.py
@@ -0,0 +1,41 @@
+import boto3
+import json
+import os
+
+s3 = boto3.client('s3')
+
+BUCKET_NAME = os.getenv('S3-BUCKET-NAME=')
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+
+        tenant_id = body.get('tenantId')
+        file_id = body.get('fileId')
+
+        if not tenant_id or not file_id:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Both tenantId and fileId are required"})
+            }
+
+        s3_key = f"{tenant_id}/{file_id}"
+
+        url = s3.generate_presigned_url(
+            ClientMethod="get_object",
+            Params={
+                "Bucket": BUCKET_NAME,
+                "Key": s3_key
+            },
+            ExpiresIn=3600
+        )
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"imageUrl": url})
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
